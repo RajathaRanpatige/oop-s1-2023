c4d4a85f7d24c549d7ba230f0a24d28e300c47ef
RajathaRanpatige <a1880587@adelaide.edu.au>
2023-07-25 10:30:12 +0930

    Second commit with added comments and test cases

--- main.cpp
+++ main.cpp
@@ -7,5 +7,14 @@ extern int count(int[], int);
 int main() {
     int array[5] = {4,5,6,7,8};
     std::cout << "The number is: " << count(array, 5) << std::endl;
+    
+    //test case 1
+    //int array[3] = {-1,2,3};
+    //std::cout << "The number is: " << count(array, 3) << std::endl;
+
+    //test case 2
+    //int array[10] = {2,4,6,8,10,-10,0,0,5,2};
+    //std::cout << "The number is: " << count(array, 10) << std::endl;
+
     return 0;
 }
