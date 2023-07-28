d44122dd821430b1071ebec0c93b3e3af6113d59
RajathaRanpatige <a1880587@adelaide.edu.au>
2023-07-28 13:53:34 +0930

    Easy 1-3

---
+++ function-1-3.cpp
@@ -0,0 +1,13 @@
+int num_count(int array[], int n, int number) {
+  int count = 0;
+
+  if (n > 0) {
+    for (int i = 0; i < n; i++) {
+      if (array[i] == number) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
