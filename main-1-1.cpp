260df18bdf5cbf759d45ae4664c274a265720868
RajathaRanpatige <a1880587@adelaide.edu.au>
2023-07-28 13:07:00 +0930

    Functionality complete

---
+++ main-1-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+extern int array_sum(int[], int);
+
+int main(void){
+
+    int arr1[5] = {0,1,2,3,4};
+    cout << array_sum(arr1,5) << "\n";
+
+    int arr2[0] = {};
+    cout << array_sum(arr2,0) << "\n";
+    
+    int arr3[4] = {-5,5,-5,-5};
+    cout << array_sum(arr3,4) << "\n";
+}
