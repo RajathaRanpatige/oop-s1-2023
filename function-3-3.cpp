fdcd1097f0fe18cd2e553c6619dd048f2c989ca5
RajathaRanpatige <a1880587@adelaide.edu.au>
2023-07-29 23:35:33 +0930

    Final final heehee

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -1,15 +1,24 @@
 double weighted_average(int array[], int n) {
-  int distinctCount = 0;
+
+  double wAvg = 0;
 
   if (n > 0){
+    
+    for (int i = 0; i < n; i++){
 
-    for(int i = 0; i < n; i++){
+      int count = 1;
 
+      for (int j = 0; j < n; j++){
+
+        if (i != j && array[i] == array[j]){
+          count++;
+        }
+      }
+
+      wAvg += array[i] * (double)count / n;
     }
 
-
-ajabsd
-
-
   }
+
+  return wAvg;
 }
