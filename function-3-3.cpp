70cb2fe94d1d324d5fb9833203a29b3c1522765c
RajathaRanpatige <a1880587@adelaide.edu.au>
2023-07-29 23:37:25 +0930

    Final final heehee

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -1,23 +1,18 @@
 double weighted_average(int array[], int n) {
-
   double wAvg = 0;
 
-  if (n > 0){
-    
-    for (int i = 0; i < n; i++){
-
+  if (n > 0) {
+    for (int i = 0; i < n; i++) {
       int count = 1;
 
-      for (int j = 0; j < n; j++){
-
-        if (i != j && array[i] == array[j]){
+      for (int j = 0; j < n; j++) {
+        if (i != j && array[i] == array[j]) {
           count++;
         }
       }
 
       wAvg += array[i] * (double)count / n;
     }
-
   }
 
   return wAvg;
--- main-3-3.cpp
+++ main-3-3.cpp
@@ -4,15 +4,14 @@ using namespace std;
 
 extern double weighted_average(int array[], int n);
 
-int main(void){
+int main(void) {
+  // Test cases
+  int arr1[6] = {1, 2, 1, 4, 1, 3};
+  cout << weighted_average(arr1, 6) << "\n";
 
-    // Test cases
-    int arr1[6] = {1,2,1,4,1,3};
-    cout << weighted_average(arr1, 6) << "\n";
+  int arr2[0] = {};
+  cout << weighted_average(arr2, 0) << "\n";
 
-    int arr2[0] = {};
-    cout << weighted_average(arr2, 0) << "\n";
-
-    int arr3[5] = {-5,-6,7,0,0};
-    cout << weighted_average(arr3, 5) << "\n";
+  int arr3[5] = {-5, -6, 7, 0, 0};
+  cout << weighted_average(arr3, 5) << "\n";
 }
