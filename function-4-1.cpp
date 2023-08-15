c855a488454aefd8efb8cbcf5bf8024e5b1286ac
RajathaRanpatige <a1880587@adelaide.edu.au>
2023-08-16 00:00:30 +0930

    phew

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -34,6 +34,7 @@ int secondSmallestSum(int *numbers, int length)
     {
         int *sums = new int[(length+1)*length/2];
         int count = 0;
+        int sum;
         for (int i = 0; i < length - 1; i++)
         {
             for (int j = 0; j < length - i; j++)
