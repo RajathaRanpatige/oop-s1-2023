29f3a1754d32c691d2d41c8c5ecec7396598822a
RajathaRanpatige <a1880587@adelaide.edu.au>
2023-10-17 21:27:47 +1030

    yeah

--- ws11-1.cpp
+++ ws11-1.cpp
@@ -20,9 +20,16 @@ public:
         _file.open(_filename, ios::in);
         if (_file.is_open()) {
             string line;
-            while (std::getline(_file, line)) {
-                records.push_back(stoi(line));
+            try{
+                while (std::getline(_file, line)) {
+                    records.push_back(stoi(line));
+                }
+            }catch (std::invalid_argument){
+                throw std::invalid_argument("Wrong argument when reading the file");
+            }catch (std::out_of_range){
+                throw std::out_of_range("Out of range when reading the file");
             }
+
             _file.close();
         }
     }
@@ -30,18 +37,24 @@ public:
 
 int main() {
         
-        RecordsManager receordM("test_clean.txt");
+        // RecordsManager receordM("test_clean.txt");
         // RecordsManager receordM("test_corrupt1.txt");
-        // RecordsManager receordM("test_corrupt2.txt");
+        RecordsManager receordM("test_corrupt2.txt");
         Records myRecords;
 
         // reads records
-        receordM.read(myRecords);
+        try{
+            receordM.read(myRecords);
+        }catch(std::invalid_argument){
+            cout << "Wrong argument when reading the file\n";
+        }catch(std::out_of_range){
+            cout << "Out of range when reading the file\n";
+        }
 
         // calculate and print out the sum
         int sum = 0;
-        for (int i = 0; i < myRecords.size(); i++) {
-             sum += myRecords[i];
+        for (int i = 0; i < (int)myRecords.size(); i++) {
+            sum += myRecords[i];
         }
         cout << sum << endl;
     return 0;
